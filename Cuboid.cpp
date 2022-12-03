3c1ca3b1a224a30cb7fb6ae7b1e7b7d0d0720639
banimania <79039680+banimania@users.noreply.github.com>
2022-12-03 19:41:04 +0100

    DFHDFHFD

--- Cuboid.cpp
+++ Cuboid.cpp
@@ -54,46 +54,16 @@ void Cuboid::render() {
 	glEnd();
 
 	//Draw points
+	Vec3 points[7]{ p1, p2, p3, p4, p5, p6, p7 };
 	glPointSize(5.0f);
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p1.x, p1.y, p1.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p2.x, p2.y, p2.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p3.x, p3.y, p3.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p4.x, p4.y, p4.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p5.x, p5.y, p5.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p6.x, p6.y, p6.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p7.x, p7.y, p7.z);
-	glEnd();
-
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(p8.x, p8.y, p8.z);
-	glEnd();
+	
+	for (int i = 0; i < sizeof(points) / sizeof(Vec3); i++) {
+		glBegin(GL_POINTS);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(points[i].x, points[i].y, points[i].z);
+		glEnd();
+	}
+	
 	glPointSize(0.0f);
 }
 
